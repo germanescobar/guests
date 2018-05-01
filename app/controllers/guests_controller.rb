cb08e9aeb488ba2266d7e0dc37fd7f6ebb80af0a
Germán Escobar <german.escobarc@gmail.com>
2018-04-30 20:01:52 -0500

    Implement add guest with AJAX

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,4 +2,18 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+    @guest.save
+  end
+
+  private
+    def guest_params
+      params.require(:guest).permit(:email, :name)
+    end
 end
